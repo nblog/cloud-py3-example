24bb8885e777e0118ae868f41a0017a6bc15916b
r0th3r <503407184@qq.com>
2023-03-04 20:28:05 +0800

    update

---
+++ vnc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, platform, urllib.request, subprocess
+
+
+HTTPGET = urllib.request.urlopen
+
+
+
+raise NotImplementedError("Not implemented yet")
