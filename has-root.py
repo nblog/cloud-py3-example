588db50414856d596d1de33f84697c127fa722f7
r0th3r <503407184@qq.com>
2023-03-07 18:03:07 +0800

    update

--- has-root.py
+++ has-root.py
@@ -18,7 +18,7 @@ def has_root():
             return (True, os.environ['SUDO_USER'])
         else:
             ''' https://docs.python.org/3/library/getpass.html#getpass.getuser '''
-            has_name = tuple(filter(os.environ.get, ("LOGNAME", "USER", "LNAME", "USERNAME")))[0]
+            has_name = list(filter(os.environ.get, ("LOGNAME", "USER", "LNAME", "USERNAME")))[0]
             return (False, os.environ[has_name])
 
 
