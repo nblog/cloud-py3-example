8a28f431fe569d0ec3e3a25aea84349a38767669
r0th3r <503407184@qq.com>
2023-03-15 10:14:32 +0800

    update

---
+++ x64dbg.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, platform, urllib.request, subprocess
+
+
+HTTPGET = urllib.request.urlopen
+
+
+class x64dbg:
+
+    RELEASES_URL = os.environ.get("GHPROXY","") + \
+        "https://github.com/x64dbg/x64dbg/releases"
+
+
+    def latest(self):
+        import re
+        res = HTTPGET( "/".join([self.RELEASES_URL, "latest"]) )
+        tagVer = re.findall(">(snapshot[0-9\x2d\x5f]+).zip</h1>", res.read().decode())[0]
+        return tagVer
+
+    def download(self, tagVer="latest"):
+        if tagVer == "latest": tagVer = self.latest()
+        downUrl = "/".join([
+            self.RELEASES_URL, "download", f"snapshot/{tagVer}.zip"])
+        resp = HTTPGET(downUrl)
+        if (200 == resp.status):
+            return self.extract(resp.read(), os.path.basename(downUrl))
+
+        raise Exception("download failed: " + downUrl)
+
+    def extract(self, data=b'', target=''):
+        import io, zipfile
+        zipfile.ZipFile(io.BytesIO(data)).extractall()
+        return os.getcwd()
+
+
+if __name__ == "__main__":
+    x64dbg().download()
