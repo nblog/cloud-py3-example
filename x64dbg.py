7295bd46022f5cf282a3063b9f49eecbe1c9b37f
r0th3r <503407184@qq.com>
2023-03-15 10:40:21 +0800

    update

--- x64dbg.py
+++ x64dbg.py
@@ -35,5 +35,33 @@ class x64dbg:
         return os.getcwd()
 
 
+class die_engine:
+
+    RELEASES_URL = os.environ.get("GHPROXY","") + \
+        "https://github.com/horsicq/DIE-engine/releases"
+
+
+    def latest(self):
+        import re
+        res = HTTPGET( "/".join([self.RELEASES_URL, "latest"]) )
+        tagVer = str(res.url).split("tag/")[-1]
+        return tagVer
+
+    def download(self, tagVer="latest"):
+        if tagVer == "latest": tagVer = self.latest()
+        downUrl = "/".join([
+            self.RELEASES_URL, "download", tagVer,
+            f"die_win64_qt6_portable_{tagVer}.zip"])
+        resp = HTTPGET(downUrl)
+        if (200 == resp.status):
+            return self.extract(resp.read(), os.path.basename(downUrl))
+
+        raise Exception("download failed: " + downUrl)
+
+    def extract(self, data=b'', target=''):
+        import io, zipfile
+        zipfile.ZipFile(io.BytesIO(data)).extractall()
+        return os.getcwd()
+
 if __name__ == "__main__":
     x64dbg().download()
