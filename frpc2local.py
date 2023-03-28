20ff886e7bdead746c021b7c7a2f73602370cce7
r0th3r <503407184@qq.com>
2023-03-28 12:20:59 +0800

    update

--- frpc2local.py
+++ frpc2local.py
@@ -12,18 +12,18 @@ if __name__ == "__main__":
 
     ''' has root '''
     has_root = "has-root.py"
-    url = os.environ.get("GHPROXY","") + \
+    DOWNURL = os.environ.get("GHPROXY","") + \
         f"https://github.com/nblog/cloud-py3-example/blob/main/{has_root}?raw=true"
-    exec(HTTPGET(url).read().decode('utf-8'))
+    exec(HTTPGET(DOWNURL).read().decode('utf-8'))
 
     ''' frpc '''
     frpc = "frpc.py"
-    url = os.environ.get("GHPROXY","") + \
+    DOWNURL = os.environ.get("GHPROXY","") + \
         f"https://github.com/nblog/cloud-py3-example/blob/main/{frpc}?raw=true"
-    exec(HTTPGET(url).read().decode('utf-8'))
+    exec(HTTPGET(DOWNURL).read().decode('utf-8'))
 
     ''' frpc to local '''
     target = os.environ.get("frpc2local", "frida-server.py")
-    url = os.environ.get("GHPROXY","") + \
+    DOWNURL = os.environ.get("GHPROXY","") + \
         f"https://github.com/nblog/cloud-py3-example/blob/main/{target}?raw=true"
-    exec(HTTPGET(url).read().decode('utf-8'))
+    exec(HTTPGET(DOWNURL).read().decode('utf-8'))
