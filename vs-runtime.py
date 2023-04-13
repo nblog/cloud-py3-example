fa0abde160d959b6b230e461589b63594b0d7250
r0th3r <503407184@qq.com>
2023-04-13 10:56:37 +0800

    update

---
+++ vs-runtime.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys, re, platform, urllib.request, subprocess
+
+
+HTTPGET = urllib.request.urlopen
+
+
+B64 = bool(sys.maxsize > 2**32)
+
+
+class dotnet:
+
+    class TARGET:
+        class enum_dotnetver:
+            dotnet45 = "45"
+            dotnet451 = "451"
+            dotnet452 = "452"
+            dotnet46 = "46"
+            dotnet461 = "461"
+            dotnet462 = "462"
+            dotnet47 = "47"
+            dotnet471 = "471"
+            dotnet472 = "472"
+            dotnet48 = "48"
+            dotnet481 = "481"
+        dotnetver = enum_dotnetver.dotnet481
+
+
+    def download(self, dotnetver=TARGET.enum_dotnetver.dotnet481):
+        ''' https://dotnet.microsoft.com/download/dotnet-framework '''
+        dotnet.TARGET.dotnetver = dotnetver
+
+        downUrl = f"https://dotnet.microsoft.com/download/dotnet-framework/thank-you/net{dotnet.TARGET.dotnetver}-offline-installer"
+        resp = HTTPGET(downUrl)
+        if (200 == resp.status):
+            downUrl = re.findall("href=\"(.*?)\" onclick=", resp.read().decode("utf-8"))[0]
+            resp = HTTPGET(downUrl)
+            if (200 == resp.status):
+                open(os.path.basename(resp.url), "wb").write(resp.read())
+                return True
+
+        raise Exception("download failed: " + downUrl)
+
+    def version(self):
+        ''' https://learn.microsoft.com/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#minimum-version '''
+        import winreg
+        try:
+            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full")
+            value = winreg.QueryValueEx(key, "Release")[0]
+            if (value >= 528040):
+                return dotnet.TARGET.enum_dotnetver.dotnet48
+            elif (value >= 461808):
+                return dotnet.TARGET.enum_dotnetver.dotnet472
+            elif (value >= 461308):
+                return dotnet.TARGET.enum_dotnetver.dotnet471
+            elif (value >= 460798):
+                return dotnet.TARGET.enum_dotnetver.dotnet47
+            elif (value >= 394802):
+                return dotnet.TARGET.enum_dotnetver.dotnet462
+            elif (value >= 394254):
+                return dotnet.TARGET.enum_dotnetver.dotnet461
+            elif (value >= 393295):
+                return dotnet.TARGET.enum_dotnetver.dotnet46
+            elif (value >= 379893):
+                return dotnet.TARGET.enum_dotnetver.dotnet452
+            elif (value >= 378675):
+                return dotnet.TARGET.enum_dotnetver.dotnet451
+            elif (value >= 378389):
+                return dotnet.TARGET.enum_dotnetver.dotnet45
+            else:
+                return "not supported"
+        except:
+            return "unavailable"
+
+
+
+class vcruntime:
+
+    def download(self):
+        ''' https://learn.microsoft.com/cpp/windows/latest-supported-vc-redist#visual-studio-2015-2017-2019-and-2022 '''
+
+        downUrl = "https://aka.ms/vs/17/release/vc_redist.{}.exe" \
+            .format("x64" if(B64) else "x86")
+        resp = HTTPGET(downUrl)
+        if (200 == resp.status):
+            open(os.path.basename(resp.url), "wb").write(resp.read())
+            return True
+
+        raise Exception("download failed: " + downUrl)
+
+
+
+if __name__ == "__main__":
+
+    if "windows" != platform.system().lower():
+        raise Exception("only support windows")
+
+    dotnet().download()
+    vcruntime().download()
