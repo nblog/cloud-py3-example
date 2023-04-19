4baf3bf56a898a0800999040b911c91241817e33
r0th3r <503407184@qq.com>
2023-04-19 18:31:24 +0800

    update

--- vs-runtime.py
+++ vs-runtime.py
@@ -112,4 +112,4 @@ if __name__ == "__main__":
 
     print("dotnet version: " + ("net"+dotnet().version()))
 
-    vcruntime().download(), dotnet().download()
+    dotnet().download(), vcruntime().download()
