bc4f4b4dedf635d01ca1246b8cb236a5be894b77
r0th3r <503407184@qq.com>
2023-05-08 12:30:51 +0800

    update

--- x64dbg.py
+++ x64dbg.py
@@ -460,6 +460,14 @@ class winark:
             if (200 == resp.status):
                 return EXTRACT.zip(resp.read(), target_dir=target_dir)
 
+    class pyark:
+        def download(self, target_dir='ark'):
+            downUrl = "https://github.com/antiwar3/py" \
+                "/blob/master/Pyark.zip?raw=true"
+            resp = HTTPGET(downUrl)
+            if (200 == resp.status):
+                return EXTRACT.zip(resp.read(), target_dir=os.path.join(target_dir, "pyark"))
+
     class ke64:
         def download(self, target_dir='ark'):
             downUrl = "https://github.com/alinml/ke64" \
@@ -477,7 +485,9 @@ if __name__ == "__main__":
 
     die_engine().download()
 
-    winark.WKE().download(); winark.WKTools().download()
+    winark.WKE().download(), \
+        winark.WKTools().download(), \
+        winark.pyark().download()
 
 
     sysinternals.debugview().download(), \
