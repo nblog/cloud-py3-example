9e78551a53ff00f5b4dce0245cfd2f184a13d1fc
r0th3r <503407184@qq.com>
2023-07-28 17:18:50 +0800

    update

--- ghidra.py
+++ ghidra.py
@@ -103,11 +103,11 @@ class ghidra:
 
     def plugin(self, ghidra_dir):
         ''' ghidra plugin '''
-        def Ghidrathon(ghidra_dir):
-            ''' https://github.com/mandiant/Ghidrathon/releases/latest '''
-
         def pyhidra(ghidra_dir):
             ''' https://github.com/dod-cyber-crime-center/pyhidra/releases/latest '''
+
+        def Ghidrathon(ghidra_dir):
+            ''' https://github.com/mandiant/Ghidrathon/releases/latest '''
 
         def GhidraEmu(ghidra_dir):
             ''' https://github.com/Nalen98/GhidraEmu/releases/latest '''
