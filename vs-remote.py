a9f272fb43361a610b3c018587a9c7e50621a08f
r0th3r <503407184@qq.com>
2023-08-01 17:15:35 +0800

    update

--- vs-remote.py
+++ vs-remote.py
@@ -21,17 +21,45 @@ class vs_remote:
 
         class enum_vsver:
             vs2017, vs2019, vs2022 = 15, 16, 17
+            vs2012, vs2013, vs2015 = 11, 12, 14
+            vs2008, vs2010 = 9, 10
 
         vsver = enum_vsver.vs2022
         arch = enum_arch.AMD64
         language = enum_language.en_us
 
+    def vs2008(self):
+        return "https://download.microsoft.com/download/9/8/2/98220c80-1633-4297-8b02-a8af777057b8/rdbgsetup_x64.exe"
 
-    def download(self, vsVer=TARGET.enum_vsver.vs2022):
+    def vs2010(self):
+        return "https://download.microsoft.com/download/E/E/1/EE10FC0E-8408-4C09-B9EB-4684160CFEE2/rdbgsetup_x64.exe"
+
+    def vs2012(self):
+        return "https://download.microsoft.com/download/4/1/5/41524F91-4CEE-416B-BB70-305756373937/VSU4/rtools_setup_x64.exe"
+
+    def vs2013(self):
+        return "https://download.microsoft.com/download/6/F/8/6F8AEDBF-E027-492A-9009-DB38788BBA02/rtools_setup_x64.exe"
+
+    def vs2015(self):
+        return "https://download.microsoft.com/download/E/7/A/E7AEA696-A4EB-48DD-BA4A-9BE41A402400/rtools_setup_x64.exe"
+
+    def download(self, vsVer: int):
         vs_remote.TARGET.vsver = vsVer
 
         downUrl = f"https://aka.ms/vs/{vs_remote.TARGET.vsver}/release/" \
             f"RemoteTools.{vs_remote.TARGET.arch}ret.{vs_remote.TARGET.language}.exe"
+
+        if (vsVer == vs_remote.TARGET.enum_vsver.vs2008):
+            downUrl = self.vs2008()
+        if (vsVer == vs_remote.TARGET.enum_vsver.vs2010):
+            downUrl = self.vs2010()
+        if (vsVer == vs_remote.TARGET.enum_vsver.vs2012):
+            downUrl = self.vs2012()
+        if (vsVer == vs_remote.TARGET.enum_vsver.vs2013):
+            downUrl = self.vs2013()
+        if (vsVer == vs_remote.TARGET.enum_vsver.vs2015):
+            downUrl = self.vs2015()
+
         resp = HTTPGET(downUrl)
         if (200 == resp.status):
             target = os.path.basename(resp.url)
@@ -67,7 +95,8 @@ if __name__ == "__main__":
         "/nofirewallwarn"
     ]
 
-    cmd += ["/timeout", input("maximum idle time, seconds(default:3 hours):") or str(3 * 3600)]
+    cmd += ["/timeout", str(3 * 86400)]
+    # cmd += ["/timeout", input("maximum idle time, seconds(default:3 hours):") or str(3 * 3600)]
 
     # ''' default port is different '''
     # if ("vsremote_port" in os.environ):
