a1ef46f76858645cb1cda82ff23ec06efdfa8abe
nblog <503407184@qq.com>
2024-09-20 10:17:54 +0800

    update

--- vs-remote.py
+++ vs-remote.py
@@ -32,13 +32,17 @@ class vs_remote:
         vsver = enum_vsver.vs2022
 
     def vs2012(self):
+        return "https://go.microsoft.com/fwlink/?linkid=327554" if ARCH_ARM else "https://go.microsoft.com/fwlink/?linkid=327553"
         return "https://download.microsoft.com/download/4/1/5/41524F91-4CEE-416B-BB70-305756373937/VSU4/rtools_setup_x64.exe"
 
     def vs2013(self):
+        return "https://go.microsoft.com/fwlink/?linkid=512601" if ARCH_ARM else "https://go.microsoft.com/fwlink/?linkid=512600"
         return "https://download.microsoft.com/download/6/F/8/6F8AEDBF-E027-492A-9009-DB38788BBA02/rtools_setup_x64.exe"
 
+    # https://zzz.buzz/notes/visual-studio-remote-debugger (RTM)
     def vs2015(self):
-        return "https://download.microsoft.com/download/E/7/A/E7AEA696-A4EB-48DD-BA4A-9BE41A402400/rtools_setup_x64.exe"
+        return "https://download.microsoft.com/download/1/2/2/1225C23D-3599-48C9-A314-F7D631F43241/rtools_setup_arm.exe" \
+            if ARCH_ARM else "https://download.microsoft.com/download/1/2/2/1225C23D-3599-48C9-A314-F7D631F43241/rtools_setup_x64.exe"
 
     def has_installed(self):
         if (B64 and not ARCH_ARM):
@@ -62,6 +66,8 @@ class vs_remote:
         downUrl = f"https://aka.ms/vs/{vs_remote.TARGET.vsver}/release/" \
             f"RemoteTools.{vs_remote.TARGET.arch}ret.{vs_remote.TARGET.language}.exe"
 
+        if (vsVer < vs_remote.TARGET.enum_vsver.vs2012):
+            raise NotImplementedError("only support vs2012+")
         if (vsVer == vs_remote.TARGET.enum_vsver.vs2012):
             downUrl = self.vs2012()
         if (vsVer == vs_remote.TARGET.enum_vsver.vs2013):
