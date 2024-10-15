6644077d7cfc62f19add0af76beece7780fa3ca0
nblog <503407184@qq.com>
2024-10-15 14:00:58 +0800

    update

---
+++ ms-rdp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys, re, platform, urllib.request, subprocess
+
+
+HTTPGET = urllib.request.urlopen
+
+
+
+if __name__ == "__main__":
+
+    if 'windows' != platform.system().lower():
+        raise NotImplementedError("only support windows")
+
+    MSRDP_PORT = 3389
+
+
+    ''' reserved for frpc '''
+    os.environ["FRPC_LOCAL_PORT"] = MSRDP_PORT
