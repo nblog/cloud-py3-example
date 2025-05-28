7f060cd87dcc87775912ad81db00b8c9964c8382
nblog <503407184@qq.com>
2025-05-28 11:56:00 +0800

    update

--- x64dbg.py
+++ x64dbg.py
@@ -537,6 +537,7 @@ if __name__ == "__main__":
         sysinternals.WinObj().download(); \
         sysinternals.Testlimit().download(); \
         sysinternals.DebugView().download(); \
+        sysinternals.ZoomIt().download(); \
 
     # if (input("plug-in download? (y/n):").lower().startswith("y")):
     #     debugger.x64dbg.plugin(x64DBG)
