b6502939e792d9c314dbd3a734bacd2d28344444
nblog <503407184@qq.com>
2025-05-29 20:46:54 +0800

    update

---
+++ nodepass.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, io, sys, re, types, platform, subprocess, urllib.request
+
+HTTPGET = urllib.request.urlopen
+
+if not bool(os.environ.get("DEBUGPY_RUNNING")):
+    target = "utils/common"
+    RAW_CODE = HTTPGET(f"https://github.com/nblog/cloud-py3-example/blob/main/{target}.py?raw=true").read().decode('utf-8')
+
+    raw_module = types.ModuleType('common')
+    sys.modules['utils.common'] = raw_module
+    exec(RAW_CODE, raw_module.__dict__)
+
+from utils.common import (
+    EXTRACT, GITHUB_RELEASES
+)
+
+
+class nodepass:
+    ''' https://github.com/yosebyte/nodepass/releases '''
+
+    class TARGET:
+        system = platform.system().lower()
+        arch = dict({
+            "x86_64": "amd64", 
+            "armv8l": "arm64", "aarch64": "arm64", 
+        }).get(platform.machine().lower(), platform.machine().lower())
+
+    def download(self, target_dir="nodepass", tagVer="latest"):
+        downUrl = GITHUB_RELEASES(source="yosebyte/nodepass").geturl(f"nodepass_.*?_{nodepass.TARGET.system}_{nodepass.TARGET.arch}.*?", tagVer)
+        resp = HTTPGET(downUrl)
+        if (200 == resp.status):
+            return EXTRACT.extract(resp.read(), target_dir=target_dir)
+        raise Exception("download failed: " + downUrl)
+
+    def run(self, argv=[], target_dir="."):
+        binpath = os.path.abspath(os.path.join(target_dir, "nodepass"))
+        self.app = subprocess.Popen([binpath]+argv)
+
+
+
+if __name__ == "__main__":
+
+    if "NP_SERVER_ADDRESS" not in os.environ \
+        or "NP_SERVER_PORT" not in os.environ:
+        print("error:",
+            " `NP_SERVER_ADDRESS`"
+            " `NP_SERVER_PORT`"
+            " must be set\n"); exit(1)
+
+    if "EXEC_LOCAL_PORT" in os.environ:
+        os.environ["NP_LOCAL_PORT"] = os.environ["EXEC_LOCAL_PORT"]
+
+    NP_LOCAL_PORT = os.environ.get("NP_LOCAL_PORT") \
+        or input("local port you want to be converted:")
+
+    tunnel_addr = f"{os.environ['NP_SERVER_ADDRESS']}:{os.getenv('NP_SERVER_PORT', '10101')}"
+    target_addr = f":{NP_LOCAL_PORT}"
+
+    app = nodepass(); app.run([f"client://{tunnel_addr}/{target_addr}"], app.download())
